3e9bc9f888975738556cf2e9e269c78eb86d8f51
s9e <s9e.dev@gmail.com>
2014-11-05 23:38:30 +0100

    Added build script

---
+++ scripts/build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+tmpdir="/tmp"
+extdir="$tmpdir/ext"
+releasename="s9e-mediaembed-$(date -u +%Y%m%d)"
+dir="$extdir/s9e/mediaembed"
+
+files="
+	composer.json 
+	config/services.yml
+	event/subscriber.php
+	ext.php
+	LICENSE
+	README.md
+	vendor/s9e/TextFormatter/LICENSE
+	vendor/s9e/TextFormatter/README.md
+	vendor/s9e/TextFormatter/src/autoloader.php
+	vendor/s9e/TextFormatter/src/Bundle.php
+	vendor/s9e/TextFormatter/src/Bundles/MediaPack/Renderer.php
+	vendor/s9e/TextFormatter/src/Bundles/MediaPack.php
+	vendor/s9e/TextFormatter/src/Parser/BuiltInFilters.php
+	vendor/s9e/TextFormatter/src/Parser/Logger.php
+	vendor/s9e/TextFormatter/src/Parser/Tag.php
+	vendor/s9e/TextFormatter/src/Parser.php
+	vendor/s9e/TextFormatter/src/Plugins/ParserBase.php
+	vendor/s9e/TextFormatter/src/Plugins/MediaEmbed/Parser.php
+	vendor/s9e/TextFormatter/src/Renderer.php
+	vendor/s9e/TextFormatter/src/Unparser.php
+";
+
+rootdir="$(realpath $(dirname $(dirname $0)))"
+cd "$rootdir"
+
+rm -rf "$extdir"
+for file in $files;
+do
+	targetdir="$dir/$(dirname $file)"
+	mkdir -p "$targetdir"
+	cp "$file" "$targetdir"
+done
+
+cd "$tmpdir"
+kzip -r -y "$rootdir/releases/$releasename" ext
+
+rm -rf "$extdir"
