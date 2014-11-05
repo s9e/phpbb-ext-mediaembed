edd91c2f6786584308a5383f341ebba9cbc0ddd6
s9e <s9e.dev@gmail.com>
2014-11-05 23:46:44 +0100

    Removed unnecessary files from the release archive

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,26 +5,24 @@ releasename="s9e-mediaembed-$(date -u +%Y%m%d)"
 dir="$extdir/s9e/mediaembed"
 
 files="
-	composer.json 
+	LICENSE
+	README.md
 	config/services.yml
 	event/subscriber.php
 	ext.php
-	LICENSE
-	README.md
 	vendor/s9e/TextFormatter/LICENSE
-	vendor/s9e/TextFormatter/README.md
-	vendor/s9e/TextFormatter/src/autoloader.php
 	vendor/s9e/TextFormatter/src/Bundle.php
-	vendor/s9e/TextFormatter/src/Bundles/MediaPack/Renderer.php
 	vendor/s9e/TextFormatter/src/Bundles/MediaPack.php
+	vendor/s9e/TextFormatter/src/Bundles/MediaPack/Renderer.php
+	vendor/s9e/TextFormatter/src/Parser.php
 	vendor/s9e/TextFormatter/src/Parser/BuiltInFilters.php
 	vendor/s9e/TextFormatter/src/Parser/Logger.php
 	vendor/s9e/TextFormatter/src/Parser/Tag.php
-	vendor/s9e/TextFormatter/src/Parser.php
-	vendor/s9e/TextFormatter/src/Plugins/ParserBase.php
 	vendor/s9e/TextFormatter/src/Plugins/MediaEmbed/Parser.php
+	vendor/s9e/TextFormatter/src/Plugins/ParserBase.php
 	vendor/s9e/TextFormatter/src/Renderer.php
 	vendor/s9e/TextFormatter/src/Unparser.php
+	vendor/s9e/TextFormatter/src/autoloader.php
 ";
 
 rootdir="$(realpath $(dirname $(dirname $0)))"
