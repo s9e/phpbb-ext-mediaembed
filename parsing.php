2fa0c13e0e30cfcc1f1fa8c4609576bbd1ac8076
JoshyPHP <s9e.dev@gmail.com>
2016-01-29 21:07:56 +0100

    Updated site definitions

--- parsing.php
+++ parsing.php
@@ -2,7 +2,7 @@
 
 /*
 * @package   s9e\TextFormatter
-* @copyright Copyright (c) 2010-2015 The s9e Authors
+* @copyright Copyright (c) 2010-2016 The s9e Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\TextFormatter;
@@ -1026,7 +1026,7 @@ class Parser
 
 /*
 * @package   s9e\TextFormatter
-* @copyright Copyright (c) 2010-2015 The s9e Authors
+* @copyright Copyright (c) 2010-2016 The s9e Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\TextFormatter\Parser;
@@ -1275,7 +1275,7 @@ class BuiltInFilters
 
 /*
 * @package   s9e\TextFormatter
-* @copyright Copyright (c) 2010-2015 The s9e Authors
+* @copyright Copyright (c) 2010-2016 The s9e Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\TextFormatter\Parser;
@@ -1340,7 +1340,7 @@ class Logger
 
 /*
 * @package   s9e\TextFormatter
-* @copyright Copyright (c) 2010-2015 The s9e Authors
+* @copyright Copyright (c) 2010-2016 The s9e Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\TextFormatter\Parser;
@@ -1530,15 +1530,17 @@ class Tag
 
 /*
 * @package   s9e\TextFormatter
-* @copyright Copyright (c) 2010-2015 The s9e Authors
+* @copyright Copyright (c) 2010-2016 The s9e Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\TextFormatter\Plugins\MediaEmbed;
+use s9e\TextFormatter\Utils\Http;
 use s9e\TextFormatter\Parser as TagStack;
 use s9e\TextFormatter\Parser\Tag;
 use s9e\TextFormatter\Plugins\ParserBase;
 class Parser extends ParserBase
 {
+	protected static $client;
 	public function parse($text, array $matches)
 	{
 		foreach ($matches as $m)
@@ -1616,6 +1618,13 @@ class Parser extends ParserBase
 		while ($host > '');
 		return \false;
 	}
+	protected static function getHttpClient()
+	{
+		if (!isset(self::$client))
+			self::$client = Http::getClient();
+		self::$client->timeout = 10;
+		return self::$client;
+	}
 	protected static function replaceTokens($url, array $vars)
 	{
 		return \preg_replace_callback(
@@ -1664,25 +1673,20 @@ class Parser extends ParserBase
 	}
 	protected static function wget($url, $cacheDir = \null)
 	{
-		$contextOptions = array(
-			'http' => array('user_agent'  => 'PHP (not Mozilla)'),
-			'ssl'  => array('verify_peer' => \false)
-		);
-		$prefix = $suffix = '';
-		if (\extension_loaded('zlib'))
-		{
-			$prefix = 'compress.zlib://';
-			$suffix = '.gz';
-			$contextOptions['http']['header'] = 'Accept-Encoding: gzip';
-		}
+		$prefix = '';
 		if (isset($cacheDir) && \file_exists($cacheDir))
 		{
-			$cacheFile = $cacheDir . '/http.' . \crc32($url) . $suffix;
+			$cacheFile = $cacheDir . '/http.' . \crc32($url);
+			if (\extension_loaded('zlib'))
+			{
+				$prefix     = 'compress.zlib://';
+				$cacheFile .= '.gz';
+			}
 			if (\file_exists($cacheFile))
 				return \file_get_contents($prefix . $cacheFile);
 		}
-		$content = @\file_get_contents($prefix . $url, \false, \stream_context_create($contextOptions));
-		if (isset($cacheFile) && $content !== \false)
+		$content = @self::getHttpClient()->get($url, array('User-Agent: PHP (not Mozilla)'));
+		if (isset($cacheFile) && !empty($content))
 			\file_put_contents($prefix . $cacheFile, $content);
 		return $content;
 	}
@@ -1690,7 +1694,7 @@ class Parser extends ParserBase
 
 /*
 * @package   s9e\TextFormatter
-* @copyright Copyright (c) 2010-2015 The s9e Authors
+* @copyright Copyright (c) 2010-2016 The s9e Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\TextFormatter\Plugins;
